65ddb2cc0c247d1f774d3584d24b1352c2db31b4
Marcelo Prestes <marcelo.prestes@gmail.com>
2023-06-22 22:35:46 -0300

    Adicionado um paragrafo a pagina de registro

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -1,5 +1,6 @@
 <div>
     <h1>Registro</h1>
+    <p>Um paragrafo</p>
     <form wire:submit.prevent="submit">
         {{ $this->form }}
         <x-filament-support::button type="submit" wire:target="submit" class="my-4">Salvar</x-filament-support::button>
