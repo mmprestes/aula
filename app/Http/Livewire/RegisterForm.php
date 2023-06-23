f6f0f0d4e2402f88a62fd8c99725d101ca097fdf
Marcelo Prestes <marcelo.prestes@gmail.com>
2023-06-22 21:55:59 -0300

    Novos campos de endereco

--- app/Http/Livewire/RegisterForm.php
+++ app/Http/Livewire/RegisterForm.php
@@ -6,6 +6,8 @@ use Filament\Forms\Components\Grid;
 use Filament\Forms;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
@@ -27,7 +29,8 @@ class RegisterForm extends Component implements Forms\Contracts\HasForms
         return view('livewire.register-form');
     }
 
-    public function mount(): void {
+    public function mount(): void
+    {
         $this->form->fill([
             'first_name' => Fake()->firstName(),
             'last_name' => Fake()->lastName(),
@@ -41,49 +44,66 @@ class RegisterForm extends Component implements Forms\Contracts\HasForms
         ]);
     }
 
-    protected function getFormSchema(): array {
+    protected function getFormSchema(): array
+    {
         return [
-            Grid::make()
-                ->schema([
-                    TextInput::make('email')
-                        ->label('E-mail')
-                        ->email()
-                        ->required()
-                        ->columnSpanFull(),
-                    TextInput::make('first_name')
-                        ->rule('min:3')
-                        ->label('Nome')
-                        ->required(),
-                    TextInput::make('last_name')
-                        ->label('Sobrenome')
-                        ->required(),
-                    TextInput::make('company')
-                        ->label('Empresa')
-                        ->required(),
-                    TextInput::make('phone_number')
-                        ->label('Telefone')
-                        ->required(),
-                    TextInput::make('website')
-                        ->label('Site')
-                        ->url()
-                        ->required(),
-                    TextInput::make('unique_visitors')
-                        ->label('Visitantes Únicos')
-                        ->numeric()
-                        ->required(),
-                    TextInput::make('password')
-                        ->confirmed()
-                        ->label('Senha')
-                        ->password()
-                        ->required(),
-                    TextInput::make('password_confirmation')
-                        ->label('Confirme a senha')
-                        ->password()
-                        ->required(),
-                    Checkbox::make('terms')
-                        ->label('Eu concordo com os termos e condições.')
-                        ->columnSpanFull(),
-                ]),
+            Wizard::make([
+                Step::make('Conta')
+                    ->schema([
+                        TextInput::make('email')
+                            ->label('E-mail user')
+                            ->email()
+                            ->required()
+                            ->columnSpanFull(),
+                        TextInput::make('first_name')
+                            ->rule('min:3')
+                            ->label('Nome')
+                            ->required(),
+                        TextInput::make('last_name')
+                            ->label('Sobrenome')
+                            ->required(),
+                        TextInput::make('company')
+                            ->label('Empresa')
+                            ->required(),
+                        TextInput::make('phone_number')
+                            ->label('Telefone')
+                            ->required(),
+                        TextInput::make('website')
+                            ->label('Site')
+                            ->url()
+                            ->required(),
+                        TextInput::make('unique_visitors')
+                            ->label('Visitantes Únicos')
+                            ->numeric()
+                            ->required(),
+                        TextInput::make('password')
+                            ->confirmed()
+                            ->label('Senha')
+                            ->password()
+                            ->required(),
+                        TextInput::make('password_confirmation')
+                            ->label('Confirme a senha')
+                            ->password()
+                            ->required(),
+                        Checkbox::make('terms')
+                            ->label('Eu concordo com os termos e condições.')
+                            ->columnSpanFull(),
+                    ])
+                    ->columns(2),
+                     Step::make('Endereço')
+                    ->schema([
+                        TextInput::make('postal_code')
+                            ->label('CEP')
+                            ->required()
+                            ->columnSpan(2),
+                        TextInput::make('street')
+                            ->label('Logradouro')
+                            ->required()
+                            ->columnSpan(2),
+                        
+                    ]),
+            ])
+            ->startOnStep(2),
         ];
     }
 
@@ -91,7 +111,8 @@ class RegisterForm extends Component implements Forms\Contracts\HasForms
      * Summary of submit
      * @return void
      */
-    public function submit(): void {
+    public function submit(): void
+    {
         // sleep(1);
         $this->data = $this->form->getState();
     }
